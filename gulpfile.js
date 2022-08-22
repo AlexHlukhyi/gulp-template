f324b9186be049c9e143a8543b85989790cfd63f
AlexHlukhyi <oleksandr@fluidweb.io>
2022-08-22 12:53:33 +0300

    reworked template

--- gulpfile.js
+++ gulpfile.js
@@ -1,141 +1,313 @@
-const 
-	gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	browsersync = require('browser-sync').create(),
-	del = require('del'),
-	jade2 = require('gulp-jade'),
-	rename = require('gulp-rename'),
-	cssnano = require('gulp-cssnano'),
-	cleanCSS = require('gulp-clean-css'),
-	notify = require("gulp-notify");
+'use strict';
 
-function browserSync(done) {
-	browsersync.init({
-	server: {
-		baseDir: "./app"
-	},
-		notify: false
-	});
-	done();
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const changed = require('gulp-changed');
+const prettier = require('gulp-prettier');
+const beautify = require('gulp-jsbeautifier');
+const sourcemaps = require('gulp-sourcemaps');
+const hash_src = require('gulp-hash-src');
+const posthtml = require('gulp-posthtml');
+const include = require('posthtml-include');
+const richtypo = require('posthtml-richtypo');
+const expressions = require('posthtml-expressions');
+const removeAttributes = require('posthtml-remove-attributes');
+const { quotes, sectionSigns, shortWords } = require('richtypo-rules-ru');
+
+/**
+ * Основные переменные
+ */
+const paths = {
+  dist: './dist',
+  app: './app',
+  maps: './maps',
+};
+
+const src = {
+  html: paths.app + '/pages/*.html',
+  templates: paths.app + '/templates/**/*.html',
+  img: paths.app + '/img/**/*.*',
+  css: paths.app + '/css',
+  scss: paths.app + '/scss',
+  js: paths.app + '/js',
+  fonts: paths.app + '/fonts',
+  public: paths.app + '/public',
+};
+
+const dist = {
+  img: paths.dist + '/img/',
+  css: paths.dist + '/css/',
+  js: paths.dist + '/js/',
+  fonts: paths.dist + '/fonts/',
+};
+
+/**
+ * Получение аргументов командной строки
+ * @type {{}}
+ */
+const arg = ((argList) => {
+  let arg = {},
+    a,
+    opt,
+    thisOpt,
+    curOpt;
+  for (a = 0; a < argList.length; a++) {
+    thisOpt = argList[a].trim();
+    opt = thisOpt.replace(/^\-+/, '');
+
+    if (opt === thisOpt) {
+      // argument value
+      if (curOpt) arg[curOpt] = opt;
+      curOpt = null;
+    } else {
+      // argument name
+      curOpt = opt;
+      arg[curOpt] = true;
+    }
+  }
+
+  return arg;
+})(process.argv);
+
+/**
+ * Очистка папки dist перед сборкой
+ * @returns {Promise<string[]> | *}
+ */
+function clean() {
+  return del([paths.dist]);
 }
 
-function reload(done) {
-	browsersync.reload();
-	done();
+/**
+ * Инициализация веб-сервера browserSync
+ * @param done
+ */
+function browserSyncInit(done) {
+  browserSync.init({
+    server: {
+      baseDir: paths.dist,
+    },
+    host: 'localhost',
+    port: 9000,
+    logPrefix: 'log',
+  });
+  done();
 }
 
-function compileCSSlibs() {
-	return gulp.src('app/css/libs/*.css')
-		.pipe(concat('libs.css'))
-		.pipe(cssnano())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('app/css'));
+/**
+ * Функция перезагрузки страницы при разработке
+ * @param done
+ */
+function browserSyncReload(done) {
+  browserSync.reload();
+  done();
 }
 
-function compileScripts() {
-	return gulp.src([
-		'app/js/libs/jquery-3.6.0.min.js',
-		'app/js/libs/popper.min.js',
-		'app/js/libs/bootstrap.min.js'
-	])
-	.pipe(concat('libs.min.js'))
-	.pipe(gulp.dest('app/js'));
+/**
+ * Копирование шрифтов
+ * @returns {*}
+ */
+function copyFonts() {
+  return gulp.src([src.fonts + '/**/*']).pipe(gulp.dest(dist.fonts));
 }
 
-function compileSass(done) {
-  return gulp.src('app/sass/main.sass')
-    .pipe(sass().on("error", notify.onError()))
-    .pipe(rename({suffix: '.min', prefix : ''}))
-    .pipe(autoprefixer(['last 15 versions', '>1%', 'ie 8', 'ie 7'], { cascade: true }))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('app/css'))
-	.pipe(browsersync.stream());
-  
-	done();
+/**
+ * Шаблонизация и склейка HTML
+ * @returns {*}
+ */
+function htmlProcess() {
+  return gulp
+    .src([src.html])
+    .pipe(
+      posthtml([
+        include(),
+        expressions(),
+        richtypo({
+          attribute: 'data-typo',
+          rules: [quotes, sectionSigns, shortWords],
+        }),
+        removeAttributes([
+          // The only non-array argument is also possible
+          'data-typo',
+        ]),
+      ]),
+    )
+    .pipe(gulp.dest(paths.dist));
 }
 
-function compileJade2(done) {
-  	return gulp.src('app/jade/**/*.jade')
-	.pipe(
-		jade2({
-			pretty: true
-		}).on('error', notify.onError(function (error) {
-			return 'An error occurred while compiling jade.\nLook in the console for details.\n' + error;
-		}))
-	)
-    .pipe(gulp.dest('app'))
-    .pipe(browsersync.stream());
-  
-	done();
+/**
+ * Добавление хеша скриптов и стилей в html для бустинга кеша
+ * @returns {*}
+ */
+function hashProcess() {
+  return gulp
+    .src(paths.dist + '/*.html')
+    .pipe(
+      hash_src({
+        build_dir: paths.dist,
+        src_path: paths.dist + '/js',
+        exts: ['.js'],
+      }),
+    )
+    .pipe(
+      hash_src({
+        build_dir: './dist',
+        src_path: paths.dist + '/css',
+        exts: ['.css'],
+      }),
+    )
+    .pipe(gulp.dest(paths.dist));
 }
 
-function watchFiles(done) {
-	gulp.watch('app/sass/**/*.sass', gulp.series(compileSass));
-	gulp.watch('app/jade/**/*.jade', gulp.series(compileJade2));
-
-	gulp.watch('app/*.html', browsersync.reload);
-	gulp.watch('app/css/*.css', browsersync.reload);
-	gulp.watch('app/js/**/*.js', browsersync.reload);
-  
-  	done();
+/**
+ * Копирование картинок в dist или оптимизация при финишной сборке
+ * @returns {*}
+ */
+function imgProcess() {
+  return gulp
+    .src(src.img)
+    .pipe(changed(dist.img))
+    .pipe(gulp.dest(dist.img));
 }
 
-
-function removeDist(done) {
-	del.sync('dist');
-	done();
+/**
+ * Склейка CSS библиотек с минификацией
+ * @returns {*}
+ */
+function libsCssProcess() {
+  return gulp
+    .src(['node_modules/bootstrap/dist/css/bootstrap.min.css'])
+    .pipe(concat('libs.min.css'))
+    .pipe(gulp.dest(dist.css));
 }
 
-function moveFontsToDist(done) {
-	return gulp.src([
-		'app/fonts/**/*'
-	]).pipe(gulp.dest('dist/fonts'));
-	done();
+/**
+ * Склейка и обработка css файлов
+ * @returns {*}
+ */
+function cssProcess() {
+  let plugins;
+  if (arg.production === 'true') {
+    plugins = [autoprefixer(), cssnano()];
+  } else {
+    plugins = [];
+  }
+  return gulp
+    .src([src.css + '/reset.css', src.css + '/**/*.*'])
+    .pipe(concat('app.min.css'))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest(dist.css));
 }
 
-function moveImgToDist(done) {
-	return gulp.src([
-		'app/img/*',
-		'app/img/**/*'
-	]).pipe(gulp.dest('dist/img'));
-	done();
+/**
+ * Склейка и обработка scss файлов
+ * @returns {*}
+ */
+function scssProcess() {
+  const plugins = [autoprefixer({ grid: true })];
+  if (arg.production === 'true') {
+    return gulp
+      .src([src.scss + '/app.scss'])
+      .pipe(sass())
+      .pipe(postcss(plugins))
+      .pipe(prettier())
+      .pipe(gulp.dest(dist.css));
+  } else {
+    return gulp
+      .src([src.scss + '/app.scss'])
+      .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(postcss(plugins))
+      .pipe(sourcemaps.write(paths.maps))
+      .pipe(gulp.dest(dist.css));
+  }
 }
 
-function moveHtmlToDist(done) {
-	return gulp.src('app/*.html').pipe(gulp.dest('dist'));
-	done();
+/**
+ * Склейка JS библиотек с минификацией и babel
+ * @returns {*}
+ */
+function libsJsProcess() {
+  return gulp
+    .src([
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/@popperjs/core/dist/umd/popper.min.js',
+      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(babel())
+    .pipe(uglify({ output: { quote_keys: true, ascii_only: true } }))
+    .pipe(gulp.dest(dist.js));
 }
 
-function moveCssToDist(done) {
-	return gulp.src([ 
-		'app/css/*.css'
-	]).pipe(gulp.dest('dist/css'));
-
-	done();
+/**
+ * Работа с пользовательским js
+ * @returns {*}
+ */
+function jsProcess() {
+  if (arg.production === 'true') {
+    return gulp
+      .src([src.js + '/**/*.js'])
+      .pipe(beautify())
+      .pipe(babel())
+      .pipe(prettier())
+      .pipe(gulp.dest(dist.js));
+  } else {
+    return gulp
+      .src([src.js + '/**/*.js'])
+      .pipe(babel())
+      .pipe(gulp.dest(dist.js));
+  }
 }
 
-function moveJSToDist(done) {
-	return gulp.src([
-		'app/js/*.js'
-	]).pipe(gulp.dest('dist/js'));
-
-	done();
+/**
+ * Копирование файлов из папки public в корень сайта при сборке
+ * @returns {*}
+ */
+function publicProcess() {
+  return gulp
+    .src([src.public + '/**/*.*', src.public + '/**/.*'])
+    .pipe(gulp.dest(paths.dist));
 }
 
-gulp.task('build',  
-	gulp.series(
-		removeDist,compileSass, compileCSSlibs, compileScripts, compileJade2,
-		moveFontsToDist, moveImgToDist, moveHtmlToDist, moveCssToDist, moveJSToDist
-	), function(done) {
-		done();
-	}
+/**
+ * Наблюдение за изменениями в файлах
+ */
+function watchFiles() {
+  gulp.watch(src.html, gulp.series(htmlProcess, browserSyncReload));
+  gulp.watch(src.templates, gulp.series(htmlProcess, browserSyncReload));
+  gulp.watch(src.css, gulp.series(cssProcess, browserSyncReload));
+  gulp.watch(src.scss + '/**/*.*', gulp.series(scssProcess, browserSyncReload));
+  gulp.watch(src.js + '/**/*.js', gulp.series(jsProcess, browserSyncReload));
+  gulp.watch(src.img, gulp.series(imgProcess, browserSyncReload));
+  gulp.watch(src.fonts, gulp.series(copyFonts, browserSyncReload));
+  gulp.watch(src.public, gulp.series(publicProcess, browserSyncReload));
+}
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(
+    htmlProcess,
+    cssProcess,
+    libsCssProcess,
+    libsJsProcess,
+    jsProcess,
+    scssProcess,
+    imgProcess,
+    copyFonts,
+    publicProcess,
+  ),
+  hashProcess,
 );
 
-const taskBuild = gulp.series('build');
-const taskWatch = gulp.series(compileScripts, compileCSSlibs, watchFiles, browserSync);
+const watch = gulp.parallel(build, watchFiles, browserSyncInit);
 
-exports.build = taskBuild;
-exports.default = taskWatch;
+exports.build = build;
+exports.default = watch;
